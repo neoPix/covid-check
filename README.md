77d3445e90b5fa18d4e20147557b303b6b72ae56
neoPix <david@balandavid.com>
2021-05-19 15:42:21 +0200

    chore(init): managing profiles

--- README.md
+++ README.md
@@ -1,28 +1,32 @@
 # CovidCheck
 
-Trouve un rendez-vous covid près de chez vous et vous notifie via PushBullet.
+Recherche les RDV disponibles pour plusieurs profiles et les notifient par Email ou PushBullet. Un fichier de profil permet de définir les centres à surveiller pour chaque profil.
 
 ## Get it
 
 ```bash
-git clone https://gist.github.com/27c19dc989a7f87eda4fd54f4523afb4.git covidcheck
-cd covidcheck
+git clone https://github.com/neoPix/covid-check.git
+cd covid-check
 npm i
 ```
 
 ## Configure and run
 
-- Commencez par personnaliser la liste des centres `CENTERS` avec ceux autours de chez vous.
-- Configurez votre `PUSH_BULLET_TOKEN`.
-- Pensez a mofifier la fonction `typeFilter` pour filtrer correctement sur le type de vaccin souhaité.
+- Commencez par personnaliser modifier les fichier `profiles.json` avec les profiles de votre choix, leurs modes de notifications...
+- Si vous souhaitez envoyer des mails modifier `config.json`.
+- Pensez a mofifier la fonction `typeFilter` pour filtrer correctement sur le type de vaccin souhaité. A ce jour un seul filtre est disponnible pour tous les profiles.
 - `npm run start`
 
 ## AWS Lambda
 
 - `npm run build`
 - Take dist.zip
-- Entrypoint `main.main`
+- Entrypoint `main.handler`
+
+## Conception
+
+Ce code a été réalisé en observant le fonctionnement des applications Doctolib. Ce travail se basant uniquement sur de l'observation et les APIs n'étant pas documentés, il est possible que des faux positifs soient détectés.
 
 ## Context
 
-Ce code a été embarqué dans une Lambda AWS et avec un CRON Cloud Watch régulier afin de détecter et réagir au plus vite sur les disponnibilité de vaccin.
+Ce code a été embarqué dans une Lambda AWS et avec un CRON Cloud Watch régulier afin de détecter et réagir au plus vite sur les disponibilité de vaccin.
--- main.js
+++ main.js
@@ -8,75 +8,55 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ***/
 
-const axios = require('axios');
-const day = require('dayjs');
-const pushBullet = require('pushbullet');
-const { promisify } = require('util');
-
-const PUSH_BULLET_TOKEN = 'YOUR_TOKEN';
-const CENTERS = [
-    {
-        "name": "centre-de-vaccination-covid-19-chantepie",
-        "url": "https://partners.doctolib.fr/centre-de-sante/chantepie/centre-de-vaccination-covid-19-chantepie"
-    },
-    {
-        "name": "centre-de-vaccination-de-saint-gregoire",
-        "url": "https://partners.doctolib.fr/centre-de-sante/saint-gregoire/centre-de-vaccination-de-saint-gregoire"
-    },
-    {
-        "name": "centre-de-vaccination-covid-19-vaccimobile-35-ars-communes-ile-et-vilaine",
-        "url": "https://partners.doctolib.fr/centre-de-sante/ile-et-vilaine/centre-de-vaccination-covid-19-vaccimobile-35-ars-communes-ile-et-vilaine"
-    },
-    {
-        "name": "centre-de-vaccination-parc-expo-bruz",
-        "url": "https://partners.doctolib.fr/centre-de-sante/bruz/centre-de-vaccination-parc-expo-bruz"
-    },
-    {
-        "name": "centre-de-vaccination-covid-19-de-la-cpts-de-bretagne-romantique",
-        "url": "https://partners.doctolib.fr/centre-de-sante/combourg/centre-de-vaccination-covid-19-de-la-cpts-de-bretagne-romantique"
-    },
-    {
-        "name": "centre-de-vaccination-covid-19-de-liffre",
-        "url": "https://partners.doctolib.fr/centre-de-sante/liffre/centre-de-vaccination-covid-19-de-liffre"
-    },
-    {
-        "name": "centre-de-vaccination-covid-19-rennes-liberte",
-        "url": "https://partners.doctolib.fr/centre-de-vaccinations-internationales/rennes/centre-de-vaccination-covid-19-rennes-liberte"
-    },
-    {
-        "name": "centre-de-vaccination-covid-19-sos-medecins-rennes",
-        "url": "https://partners.doctolib.fr/centre-de-sante/rennes/centre-de-vaccination-covid-19-sos-medecins-rennes"
-    },
-    {
-        "name": "centre-de-vaccination-covid-chateaubriant",
-        "url": "https://partners.doctolib.fr/cabinet-medical/chateaubriant/centre-de-vaccination-covid-chateaubriant"
-    },
-    {
-        "name": "centre-de-vaccination-de-fougeres",
-        "url": "https://partners.doctolib.fr/centre-de-sante/fougeres/centre-de-vaccination-de-fougeres"
-    },
-    {
-        "name": "centre-de-vaccination-de-fougeres",
-        "url": "https://partners.doctolib.fr/centre-de-sante/fougeres/centre-de-vaccination-de-fougeres"
+const getEmailTransport = (() => {
+    let transporter = null;
+    return () => {
+        const nodemailer = require("nodemailer");
+        const config = require('./config.json'); 
+        if (!transporter) {
+            transporter = nodemailer.createTransport({
+                host: config.email.host,
+                port: config.email.port,
+                secure: config.email.secure,
+                auth: {
+                    user: config.email.user,
+                    pass: config.email.password
+                },
+            });
+        }
+        return transporter;
     }
-];
+})();
 
-async function getCenterInformation(centerSlug) {
-    const { data: { data: center } } = await axios.get(`https://partners.doctolib.fr/booking/${centerSlug}.json`);
-    return {
-        centerId: center.profile.id,
-        visitMotives: center.visit_motives.map(({ id, name }) => ({ id, name })),
-        agendas: center.agendas.map(({ id, visit_motive_ids, practice_id }) => ({ id, visitMotives: visit_motive_ids, place: practice_id })),
-        name: center.profile.name_with_title,
-    }
+async function getCenterInformations(url) {
+    const axios = require('axios');
+    const { data: { data: { profile: { id } } } } = await axios.get(`${url}.json`);
+    const { data: { data: center } } = await axios.get(`https://partners.doctolib.fr/booking/${id}.json`);
+    const availableAgendas = center.agendas.filter(({ booking_disabled, booking_temporary_disabled }) => !booking_disabled && !booking_temporary_disabled);
+    return center.places.map(({ id, formal_name, city }) => {
+        const actualId = Number(id.replace('practice-', ''));
+        const agendas = availableAgendas.filter(({ practice_id }) => practice_id === actualId);
+        const motives = center.visit_motives.filter(({ id }) => agendas.some(({ visit_motive_ids }) => visit_motive_ids.includes(id)));
+        return {
+            id: actualId,
+            url: `${url}?pid=${id}`,
+            tag: id,
+            name: formal_name,
+            agendas: agendas.map(({ id, visit_motive_ids, practice_id }) => ({ id, visitMotives: visit_motive_ids, place: practice_id })),
+            visitMotives: motives.map(({ id, name }) => ({ id, name })),
+            city
+        };
+    }).filter(({ agendas, visitMotives }) => agendas.length > 0 && visitMotives.length > 0);
 }
 
 async function getAvailableTomorrowForCenter(centerInfo, date) {
     if (centerInfo.visitMotives.length === 0) {
         return null;
     }
+    const day = require('dayjs');
     const tomorrow = (date ? day(date) : day().add(1, 'd')).format('YYYY-MM-DD');
-    const { data } = await axios.get(`https://partners.doctolib.fr/availabilities.json?start_date=${tomorrow}&visit_motive_ids=${centerInfo.visitMotives.map(({ id }) => id).join('-')}&agenda_ids=${centerInfo.agendas.map(({ id }) => id).join('-')}&insurance_sector=public&practice_ids=${centerInfo.agendas.map(({ place }) => place).join('-')}&destroy_temporary=true&limit=10`);
+    const axios = require('axios');
+    const { data } = await axios.get(`https://partners.doctolib.fr/availabilities.json?start_date=${tomorrow}&visit_motive_ids=${centerInfo.visitMotives.map(({ id }) => id).join('-')}&agenda_ids=${centerInfo.agendas.map(({ id }) => id).join('-')}&insurance_sector=public&practice_ids=${centerInfo.agendas.map(({ place }) => place).join('-')}&destroy_temporary=true&limit=1`);
 
     if (data.availabilities.length === 0) {
         return null;
@@ -103,31 +83,59 @@ function applyFilterVisitMotives(centerInfo, filter) {
 }
 
 async function getAvailabilityMap(centers, filter) {
-    return await Promise.all(centers.map(async ({ name, url }) => {
-        const centerInfo = applyFilterVisitMotives(await getCenterInformation(name), filter);
-        const result = await getAvailableTomorrowForCenter(centerInfo);
-        return {
-            available: result ? result.slots.length : 0,
-            name: centerInfo.name,
-            slug: name,
-            when: result ? result.date : '',
-            url,
-        };
+    const search = await Promise.all(centers.map(async (url) => {
+        const centers = await getCenterInformations(url);
+        return Promise.all(centers.map(async (center) => {
+            const centerInfo = applyFilterVisitMotives(center, filter);
+            const result = await getAvailableTomorrowForCenter(centerInfo);
+            return {
+                available: result ? result.slots.length : 0,
+                name: centerInfo.name,
+                when: result ? result.date : '',
+                url: centerInfo.url,
+                id: url,
+            };
+        }));
     }));
+
+    return search.flat();
 }
-async function notify(places) {
-    const bullet = new pushBullet(PUSH_BULLET_TOKEN);
+async function notifyPushBullet(profile, slots) {
+    const pushBullet = require('pushbullet');
+    const { promisify } = require('util');
+    const bullet = new pushBullet(profile.notify.token);
     const getDevices = promisify(bullet.devices.bind(bullet));
     const link = promisify(bullet.link.bind(bullet));
-    const { devices } = await getDevices({ limit: 5 });
-    const usedDevices = devices.filter(({ nickname, pushable }) => pushable && !nickname.includes('Jeedom'));
-    await Promise.all(places.map(async ({ url, available, when, name }) => {
-        const text = available > 1 ? `${available} doses sont disponnibles le ${when} à ${name}` : `${available} dose est disponnible le ${when} à ${name}`;
-        console.log('Vaccin Check', url, text)
+    const { devices } = await getDevices({ limit: 20 });
+    const usedDevices = devices.filter(({ pushable }) => pushable);
+    await Promise.all(slots.map(async ({ url, available, when, name }) => {
+        const text = available > 1 ? `Salut ${profile.name}. ${available} doses sont disponnibles le ${when} à ${name}` : `${available} dose est disponnible le ${when} à ${name}`;
         await Promise.all(usedDevices.map(async ({ iden }) => {
             await link(iden, 'Vaccin Check', url, text)
         }));
     }));
+}
+
+async function notifyEmail(profile, slots) {
+    const transport = getEmailTransport();
+    const mailBody = slots.reduce((body, { url, available, when, name }) => {
+        const text = `
+${(available > 1 ? `${available} doses sont disponnibles le ${when} à ${name}` : `${available} dose est disponnible le ${when} à ${name}.`)}
+Utilisez le lien suivant pour prendre rendez-vous ${url}.
+
+`;
+        return `${body}${text}`;
+    }, `Salut ${profile.name}.`);
+
+
+    await transport.sendMail({
+        from: config.email.user,
+        to: profile.notify.destinator,
+        subject: `Des doses sont disponnibles à ${slots.map(({name}) => name).join(', ')}`,
+        text: `${mailBody}
+
+à bientôt.`
+    });
 }
 
 const typeFilter = ({ name }) => {
@@ -136,13 +144,34 @@ const typeFilter = ({ name }) => {
 }
 
 async function main() {
-    const availability = await getAvailabilityMap(CENTERS, typeFilter);
-    const places = availability.filter(({ available }) => available > 0);
-    if (places.length > 0) {
-        notify(places);
+    const profiles = require('./profiles.json');
+    const uniqCenters = [...new Set(profiles.map(({ places }) => places).flat())];
+    const availability = await getAvailabilityMap(uniqCenters, typeFilter);
+    const slots = availability;//.filter(({ available }) => available > 0);
+    if(slots.length) {
+        console.table(slots);
+        console.info('Notifying according to profiles');
+        await Promise.all(profiles.map(async (profile) => {
+            if (!profile.notify) {
+                return console.warn(`Notification profile ${profile.name} has no notification config`);
+            }
+            const slotsToNotify = slots.filter(({ id }) => profile.places.includes(id));
+            switch (profile.notify.type) {
+                case 'pushbullet':
+                    return notifyPushBullet(profile, slotsToNotify);
+                case 'email':
+                    return notifyEmail(profile, slotsToNotify);
+                default:
+                    return console.warn(`Notification profile ${profile.name} has uses a non implemented notification ${profile.notify.type}`);
+            }
+        }));
+    } else {
+        console.info('Nothing found yet, please try again later.');
     }
 }
 
-main().catch(e => console.error(e));
+main().catch(e => console.error(e)); // A commenter pour un usage Lambda
 
-exports.main = main;
+exports.handler = async () => {
+    await main();
+};
